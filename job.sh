a340ca0567ad02e5dd3e87e3d34d822f896d8e5e
robertodeguglielmo <roberto.deguglielmo@gmail.com>
2018-07-12 18:50:36 +0200

    corrections

--- job.sh
+++ job.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-# /proc/1/fd/1 is systemd's stdout, and therefore appears in the resin logs.
-echo curl "https://www.duckdns.org/update?domains=drbertux&token=eddbeb84-a57e-4173-a2c0-e082d7f9d1d6" >/proc/1/fd/1
+curl "https://www.duckdns.org/update?domains=drbertux&token=eddbeb84-a57e-4173-a2c0-e082d7f9d1d6" >/proc/1/fd/1
